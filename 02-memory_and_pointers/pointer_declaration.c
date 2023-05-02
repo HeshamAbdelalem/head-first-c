c9874e756410e45d11cf091cab05e5724ddcf5bc
HeshamAbdelalem <hesham.abdelalem1@gmail.com>
2023-05-02 12:18:32 +0300

    Pointer declaration

---
+++ 02-memory_and_pointers/pointer_declaration.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void){
+int num = 99;
+
+int *p = &num;  // Assing [p] as a pointer to the [num] address
+
+printf("The [p] will print the memory address : %p\n", p);
+// here i can access the value of the pointer that [p] points too 
+// so [p] will print the address and [*p] will print the value of that address
+
+printf("The [*p] will print the memory address : %i\n", *p);
+
+// this will change the value of the pointers that p points too
+// so num value will be 50 and p also
+*p = 50;
+
+printf("I changed the value of [p] will print the memory address : %i\n", *p);
+
+
+return (0);
+}
